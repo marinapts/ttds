8bef04abc8e017fcab88d79576a5c9bae5ff3d12
marinapts <potsimarina@gmail.com>
2019-10-01 16:38:31 +0100

    File is not downloaded if it doesn't exist

--- lab0/bible.py
+++ lab0/bible.py
@@ -1,21 +1,27 @@
 import urllib.request
 import os.path
 
+
 def download_file_and_save(url, file_name):
-    # Download the file from `url` and save it locally under `file_name`:
-    print('Downloading file from {}...'.format(url))
-    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
-        data = response.read() # a `bytes` object
-        print('Saving file as {}...'.format(file_name))
-        out_file.write(data)
+    """Download the file from `url` and save it locally under `file_name`:
+
+    Args:
+        url (string): The url to download the file from
+        file_name (string): The name of the file
+    """
+    if not os.path.exists('./' + file_name):
+        print('Downloading file from {}...'.format(url))
+        with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+            data = response.read()  # a `bytes` object
+            print('Saving file as {}...'.format(file_name))
+            out_file.write(data)
 
 
 if __name__ == '__main__':
     URL = 'http://www.gutenberg.org/cache/epub/10/pg10.txt'
     FILENAME = 'bible.txt'
 
-    if os.path.exists(FILENAME) is False:
-        download_file_and_save(URL, FILENAME)
+    download_file_and_save(URL, FILENAME)
 
     # Open the file with read only permit
     with open('bible.txt', 'r') as f:

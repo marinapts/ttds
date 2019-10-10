c513bfbdd7c8de24beeb376e3aca03725a0cb20d
marinapts <potsimarina@gmail.com>
2019-10-10 01:24:45 +0100

    Create boolean table

--- lab2/index_search.py
+++ lab2/index_search.py
@@ -4,11 +4,15 @@ import numpy as np
 def create_boolean_search_table(inverted_index, num_of_docs):
     words = [word for word in inverted_index]
     boolean_table = np.zeros((len(words), num_of_docs))
-    print(words)
 
-    index = 0
-    for word in words:
+    for i, word in enumerate(words):
         docs_with_word = inverted_index[word].keys()
-        print(docs_with_word.keys())
-        # boolean_table[index] = [i in docs_with_word for i in ]
-        index += 1
+
+        print(docs_with_word)
+        print(docs_with_word)
+
+        for doc in docs_with_word:
+            boolean_table[i][doc - 1] = 1
+
+    print(boolean_table)
+    return boolean_table

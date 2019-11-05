ca9371e8e213a1f7e3d651623537d6f7f96de787
marinapts <potsimarina@gmail.com>
2019-11-05 00:46:10 +0000

    Add eval files for the 6 systems - 1st part

--- assignment_2/eval.py
+++ assignment_2/eval.py
@@ -1,7 +1,8 @@
+import numpy as np
 
 
 def precision(retrieved, relevant):
-    """precision at cutoff 10 (only top 10 retrieved documents in the list are considered for each query)"""
+    """precision at cutoff ` (only top 10 retrieved documents in the list are considered for each query)"""
     retrieved_and_relevant = list(set(retrieved).intersection(relevant))
     precision = len(retrieved_and_relevant) / len(retrieved)
     return precision
@@ -18,8 +19,7 @@ def avg_precision(all_retrieved, relevant):
     ap = 0
 
     for k in range(1, len(all_retrieved)):
-
-        if list(all_retrieved.keys())[k] in relevant:
+        if list(all_retrieved.keys())[k - 1] in relevant:
             k_retrieved = {c: all_retrieved[c] for c in list(all_retrieved.keys())[:k]}
             precision_k = precision(list(k_retrieved.keys()), relevant)
             ap += precision_k * 1
@@ -29,7 +29,23 @@ def avg_precision(all_retrieved, relevant):
     return ap / len(relevant)
 
 
-def nDCG(k, retrieved, relevant):
+def nDCG(retrieved, relevant):
     '''normalized discount cumulative gain at cutoff k'''
+    first_doc = retrieved[0][0]
+    rel1 = int(dict(relevant)[first_doc]) if first_doc in dict(relevant) else 0
+    DCG = rel1
+    # Calculate DCG@k
+    for doc, rank in retrieved[1:]:
+        if doc in dict(relevant):
+            DCG += int(dict(relevant)[doc]) / np.log2(int(rank))
 
-    print(retrieved)
+    # print('\n')
+    # Calculate iDCG@k
+    irel1 = int(relevant[0][1])
+    iDCG = irel1
+    # print('irel1', iDCG)
+    for idx, (doc, rel) in enumerate(relevant[1:]):
+        iDCG += int(rel) / np.log2(int(idx) + 2)
+        # print(iDCG)
+
+    return DCG / iDCG
--- assignment_2/main.py
+++ assignment_2/main.py
@@ -1,28 +1,28 @@
 import re
+import numpy as np
 from eval import precision, recall, avg_precision, nDCG
 
-
-def first_n_retrieved(all_retrieved, n):
-    first_10_docs_retrieved = {k: all_retrieved[k] for k in list(all_retrieved.keys())[:n]}
-    return first_10_docs_retrieved.keys()
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
 
-if __name__ == '__main__':
-    with open('./systems/S1.results', 'r') as f:
+def get_system_file(filename):
+    with open(filename, 'r') as f:
         lines = f.readlines()
         retrieved_docs = dict()
 
         for line in lines:
             query_id, _, doc_id, rank_of_doc, doc_score, _ = line.split(' ')
-            # doc_details = {'rank': rank_of_doc, 'score': doc_score}
             doc_details = tuple([rank_of_doc, doc_score])
 
             if query_id in retrieved_docs:
                 retrieved_docs[query_id].update({doc_id: doc_details})
             else:
                 retrieved_docs.setdefault(query_id, {doc_id: doc_details})
+    return retrieved_docs
 
-    with open('./systems/qrels.txt', 'r') as qrels_f:
+
+def get_relative_docs(filename):
+    with open(filename, 'r') as qrels_f:
         lines = qrels_f.readlines()
         relevant_docs_dict = dict()
 
@@ -40,25 +40,61 @@ if __name__ == '__main__':
                 relevant_docs_dict[query_id].update({relev_docs_list})
             else:
                 relevant_docs_dict.setdefault(query_id, relev_docs_list)
+    return relevant_docs_dict
 
-    print('Precision - Recall - r_precision - AP')
-    avg_precisions = []
-    for query in retrieved_docs:
-        first_10_retrieved = first_n_retrieved(retrieved_docs[query], 10)
-        first_50_retrieved = first_n_retrieved(retrieved_docs[query], 50)
-        relevant_docs = [x[0] for x in relevant_docs_dict[query]]
 
-        precision_10 = precision(first_10_retrieved, relevant_docs)
-        recall_50 = recall(first_50_retrieved, relevant_docs)
+def first_n_retrieved(all_retrieved, n):
+    first_10_docs_retrieved = {k: all_retrieved[k] for k in list(all_retrieved.keys())[:n]}
+    return first_10_docs_retrieved
 
-        rank_r_documents = first_n_retrieved(retrieved_docs[query], len(relevant_docs))
-        r_precision = precision(rank_r_documents, relevant_docs)
 
-        ap = avg_precision(retrieved_docs[query], relevant_docs)
-        avg_precisions.append(ap)
+def write_scores_to_file(filename, scores):
+    with open('./eval_results/' + filename + '.eval', 'w') as f:
+        column_names = ['P@10', 'R@50', 'r-Precision', 'AP', 'nDCG@10', 'nDCG@20']
+        f.write('\t' + '\t'.join(column_names) + '\n')
 
-        nDCG_10 = nDCG(10, first_10_retrieved, relevant_docs)
+        for idx, score in enumerate(scores):
+            f.write(str(idx + 1) + '\t')
+            f.write('\t'.join(format(x, ".3f") for x in score))
+            f.write('\n')
+        f.write('mean\t' + '\t'.join(format(x, ".3f") for x in np.mean(scores, axis=0)))
 
-        print('%.3f - %.3f - %.3f - %.3f' % (precision_10, recall_50, r_precision, ap))
 
-    print('MAP:', sum(avg_precisions)/len(retrieved_docs))
+if __name__ == '__main__':
+    system_files = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6']
+
+    for system_file in system_files:
+        retrieved_docs = get_system_file('./systems/' + system_file + '.results')
+        relevant_docs_dict = get_relative_docs('./systems/qrels.txt')
+
+        avg_precisions = []
+        scores = []
+
+        for query in retrieved_docs:
+            first_10_retrieved = first_n_retrieved(retrieved_docs[query], 10)
+            first_20_retrieved = first_n_retrieved(retrieved_docs[query], 20)
+            first_50_retrieved = first_n_retrieved(retrieved_docs[query], 50)
+            relevant_docs = [x[0] for x in relevant_docs_dict[query]]
+
+            precision_10 = precision(first_10_retrieved.keys(), relevant_docs)
+
+            recall_50 = recall(first_50_retrieved.keys(), relevant_docs)
+
+            rank_r_documents = first_n_retrieved(retrieved_docs[query], len(relevant_docs))
+            r_precision = precision(rank_r_documents.keys(), relevant_docs)
+
+            ap = avg_precision(retrieved_docs[query], relevant_docs)
+            avg_precisions.append(ap)
+
+            first_10_retrieved_with_rank = [(k, v[0]) for k, v in first_10_retrieved.items()]
+            nDCG_10 = nDCG(first_10_retrieved_with_rank, relevant_docs_dict[query])
+
+            first_20_retrieved_with_rank = [(k, v[0]) for k, v in first_20_retrieved.items()]
+            nDCG_20 = nDCG(first_20_retrieved_with_rank, relevant_docs_dict[query])
+
+            scores.append([precision_10, recall_50, r_precision, ap, nDCG_10, nDCG_20])
+
+        print('MAP:', sum(avg_precisions) / len(retrieved_docs))
+
+        scores = np.array(scores)
+        write_scores_to_file(system_file, scores)
